684eb9acc15953b755f157501638e48147496beb
stenya <stenyatest@gmail.com>
2019-11-22 21:20:35 +0300

    progress with car add dialog

--- app/src/main/java/by/ewoks/powervehicle/activity/SplashActivity.kt
+++ app/src/main/java/by/ewoks/powervehicle/activity/SplashActivity.kt
@@ -1,9 +1,11 @@
 package by.ewoks.powervehicle.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import by.ewoks.powervehicle.MainActivity
 import by.ewoks.powervehicle.R
 import by.ewoks.powervehicle.dialog.DialogAddCarFragment
 
@@ -15,13 +17,23 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        // show fragment to add new car if nothing exists or show nain activity with data
-        if(doesCarExists()) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        chooseWhereToGo()
+    }
+
+    // show fragment to add new car if nothing exists or show Main activity with data
+    private fun chooseWhereToGo (){
+        if(!doesCarExists()) {
             val fm = supportFragmentManager
             val addCarFragment = DialogAddCarFragment()
             addCarFragment.show(fm, "PioneersFragment_tag")
         }else{
-
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/by/ewoks/powervehicle/dialog/DialogAddCarFragment.kt
+++ app/src/main/java/by/ewoks/powervehicle/dialog/DialogAddCarFragment.kt
@@ -16,22 +16,11 @@ import by.ewoks.powervehicle.R
 class DialogAddCarFragment  : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fraglayout, container)
-        var pioneers = arrayOf("Dennis Ritchie", "Rodney Brooks", "Sergey Brin", "Larry Page", "Cynthia Breazeal", "Jeffrey Bezos", "Berners-Lee Tim", "Centaurus A", "Virgo Stellar Stream")
+        val rootView = inflater.inflate(R.layout.add_car_fragment_layout, container)
 
-        val myListView = rootView.findViewById(R.id.myListView) as ListView
-        //with arrayadapter you have to pass a textview as a resource, and that is simple_list_item_1
-        myListView!!.adapter = ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, pioneers)
 
-        this.dialog.setTitle("Tech Pioneers")
+        this.dialog!!.setTitle("AddCar")
 
-        myListView.setOnItemClickListener {
-            adapterView,
-            view,
-            position,
-            l
-            -> Toast.makeText(activity, pioneers[position], Toast.LENGTH_SHORT).show()
-        }
 
         return rootView
     }
