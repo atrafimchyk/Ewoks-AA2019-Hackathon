129ce747b2cc2a32b795830e4d15cc7cd779ba55
Lev Krasovsky <lkrasovsky.dev@gmail.com>
2019-11-22 22:33:37 +0300

    AddServiceFragment base markup.

--- app/src/main/java/by/ewoks/powervehicle/common/entities/Service.kt
+++ app/src/main/java/by/ewoks/powervehicle/common/entities/Service.kt
@@ -8,11 +8,11 @@ import androidx.room.PrimaryKey
 data class Service(
         @PrimaryKey val id: Long,
         @ColumnInfo(name = "vehicle_id") val vehicleId: Long,
-        @ColumnInfo(name = "service_type_id") val serviceTypeId: Long,
+        @ColumnInfo(name = "service_type_id") val serviceTypeId: Long,//
         @ColumnInfo(name = "date") val date: Long,
         @ColumnInfo(name = "date_of_next_service") val dateOfNextService: Long,
-        @ColumnInfo(name = "mileage") val mileage: Int,
-        @ColumnInfo(name = "name") val name: String,
-        @ColumnInfo(name = "description") val description: String,
-        @ColumnInfo(name = "service_cost") val serviceCost: Double
+        @ColumnInfo(name = "mileage") val mileage: Int, //
+        @ColumnInfo(name = "name") val name: String,//
+        @ColumnInfo(name = "description") val description: String,//
+        @ColumnInfo(name = "service_cost") val serviceCost: Double//
 )
--- app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
+++ app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
@@ -2,18 +2,35 @@ package by.ewoks.powervehicle.services
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import by.ewoks.powervehicle.Fragment
 import by.ewoks.powervehicle.R
+import kotlinx.android.synthetic.main.fragment_add_service.*
 
 class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        service_types_spinner.create()
 
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+    }
+
+    private fun Spinner.create() {
+        val array = R.array.service_types
+
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+                context,
+                array,
+                android.R.layout.simple_spinner_item
+        )
+
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        service_types_spinner.adapter = spinnerAdapter
     }
 }
