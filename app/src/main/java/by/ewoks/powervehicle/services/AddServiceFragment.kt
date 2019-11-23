27ed0de9b6695e8cb97c514773190014417cfae7
Lev Krasovsky <lkrasovsky.dev@gmail.com>
2019-11-23 05:03:29 +0300

    AddServiceFragment: hiding top elements while keyboard is open & some design changes.

--- app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
+++ app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
@@ -7,10 +7,12 @@ import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import by.ewoks.powervehicle.Fragment
 import by.ewoks.powervehicle.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_add_service.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
-
+class AddServiceFragment : Fragment(R.layout.fragment_add_service), KeyboardVisibilityEventListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         service_types_spinner.create()
@@ -19,6 +21,8 @@ class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+
+        KeyboardVisibilityEvent.setEventListener(activity, this)
     }
 
     private fun Spinner.create() {
@@ -32,5 +36,19 @@ class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
 
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         service_types_spinner.adapter = spinnerAdapter
+    }
+
+    override fun onVisibilityChanged(isOpen: Boolean) {
+        if (isOpen) {
+            activity?.bottom_navigation_view!!.visibility = View.GONE
+            title_text.visibility = View.GONE
+            service_types_spinner.visibility = View.GONE
+            service_type_title_text.visibility = View.GONE
+        } else {
+            activity?.bottom_navigation_view!!.visibility = View.VISIBLE
+            title_text.visibility = View.VISIBLE
+            service_types_spinner.visibility = View.VISIBLE
+            service_type_title_text.visibility = View.VISIBLE
+        }
     }
 }
