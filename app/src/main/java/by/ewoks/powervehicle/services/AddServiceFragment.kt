477f0ff318fdcd02682938e03c768fe5b7a521a7
Lev Krasovsky <lkrasovsky.dev@gmail.com>
2019-11-23 09:52:59 +0300

    AddServiceFragment markup.

--- app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
+++ app/src/main/java/by/ewoks/powervehicle/services/AddServiceFragment.kt
@@ -2,9 +2,14 @@ package by.ewoks.powervehicle.services
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import by.ewoks.powervehicle.Fragment
 import by.ewoks.powervehicle.R
+import by.ewoks.powervehicle.common.AppDbManager
+import kotlinx.android.synthetic.main.fragment_add_service.*
 
 class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
 
@@ -15,5 +20,20 @@ class AddServiceFragment : Fragment(R.layout.fragment_add_service) {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
+
+        val serviceTypeDao = AppDbManager.getDb().serviceTypeDao()
+        serviceTypeDao.getAllServiceTypes().observe(viewLifecycleOwner, Observer { services ->
+            val types = ArrayList<String>()
+            services.forEach {
+                types.add(it.name)
+            }
+            service_types_spinner.create(types)
+        })
+    }
+
+    private fun Spinner.create(services: ArrayList<String>) {
+        val spinnerAdapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, services)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        service_types_spinner.adapter = spinnerAdapter
     }
 }
